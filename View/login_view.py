f1cbadfe764fadfc09f0b316aca3300d69c7c96d
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-06-08 11:52:13 +0700

    feat: add auth controller

--- View/login_view.py
+++ View/login_view.py
@@ -1,46 +1,36 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QMessageBox, QWidget
 
+from controllers.auth_controller import AuthController
 from templates import loginUI
+from utils import widget
 
 
 class LoginView(QWidget):
     def __init__(self):
         super(LoginView, self).__init__()
         uic.loadUi(loginUI, self)
+        self.authController = AuthController()
 
-    #     self.login.clicked.connect(self.login)
+        self.login.clicked.connect(self.handleLogin)
+        self.signup.clicked.connect(self.changeToSignUp)
 
-    # # Xử lý khi ấn vào đăng ký
-    # def sign_up(self):
-    #     widget.setCurrentIndex(1)
+    # Xử lý khi ấn vào đăng ký
+    def changeToSignUp(self):
+        widget.setCurrentIndex(1)
 
-    # # Xử lý đăng nhập
-    # def login(self):
-    #     username = self.userName.text()
-    #     password = self.passWord.text()
+    # Xử lý đăng nhập
+    def handleLogin(self):
+        username = self.userName.text()
+        password = self.passWord.text()
 
-    #     # Kết nối tới mysql
-    #     connector = MySQLConnector(
-    #         host="localhost",
-    #         port=3306,
-    #         username="root",
-    #         password="123456",
-    #         database="qlda",
-    #     )
-    #     connector.connect()
+        print(username, password)
 
-    #     # Thực hiện câu lệnh sql
-    #     query = f"SELECT * FROM qlda.user WHERE username = '{username}' and password ='{password}'"
+        result = self.authController.login(username, password)
 
-    #     # Thực thi truy vấn SQL
-    #     result = connector.execute_query(query)
-
-    #     # Đóng kết nối với cơ sở dữ liệu MySQL
-    #     connector.close_connection()
-    #     # Kiểm tra đăng nhập
-    #     if result:
-    #         QMessageBox.information(self, "Login output", "Login succsess")
-    #         widget.setCurrentIndex(2)
-    #     else:
-    #         QMessageBox.information(self, "Login output", "Login fail")
+        # Kiểm tra đăng nhập
+        if result:
+            QMessageBox.information(self, "Login output", "Login succsess")
+            widget.setCurrentIndex(2)
+        else:
+            QMessageBox.information(self, "Login output", "Login fail")
