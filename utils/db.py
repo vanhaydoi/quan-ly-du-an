1edd6498b8b0530ed4bbebd0f7c3b2f80ffada37
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-06-08 11:29:42 +0700

    add: alembic

--- utils/db.py
+++ utils/db.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from Model.Base import Base
 
 engine = create_engine(
     "mysql+pymysql://root:newpassword@localhost/qldt_abc?charset=utf8mb4",
     echo=True,
 )
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(engine)
